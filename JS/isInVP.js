aa29fc0170678da4df66300a943c772ed148cd18
yhkekeke <diandian2510@gmail.com>
2024-04-04 15:46:57 +0800

    1. delete unuse js&html. 2. formatted js.

--- JS/isInVP.js
+++
@@ -1,8 +0,0 @@
-function isInVP(element) {
-    let scroll_top = $(window).scrollTop();
-    let element_top = $(element).offset().top;
-
-    if(scroll_top + document.documentElement.clientHeight >= element_top){
-        return true;
-    }
-}
--- JS/media.js
+++ JS/media.js
@@ -1,55 +1,55 @@
 let lastScrollTop = 0;
 // 初始化: 上次滾動位置為0
-function scrollBack(element){
-    let currentScrollTop = $(window).scrollTop();
-    // 取得本次滾動位置
+function scrollBack(element) {
+  let currentScrollTop = $(window).scrollTop();
+  // 取得本次滾動位置
 
-    if ( currentScrollTop > lastScrollTop) {
-        $(element).addClass("none");
-        // .removeClass("sticky");
-    } else {
-        $(element).removeClass("none");
-    }
+  if (currentScrollTop > lastScrollTop) {
+    $(element).addClass("none");
+    // .removeClass("sticky");
+  } else {
+    $(element).removeClass("none");
+  }
 
-    lastScrollTop = currentScrollTop;
+  lastScrollTop = currentScrollTop;
 }
 
 function isMobile() {
-    return window.innerWidth <= 768;
+  return window.innerWidth <= 768;
 }
 
-$(function(){
-    if (isMobile()){
-        $(document).scroll(function(){
-            scrollBack("header");
-            // scrollBack("aside");
-            // 這個沒成功，因為她是sticky?
-        });
-    }
+$(function () {
+  if (isMobile()) {
+    $(document).scroll(function () {
+      scrollBack("header");
+      // scrollBack("aside");
+      // 這個沒成功，因為她是sticky?
+    });
+  }
 })
 
-$(function(){
-    $("iconify-icon").each(function(){
-        var point = window.innerWidth;
-        console.log(point);
-        let $icon = $(this).find('svg');
-        let iconW = $icon.width(); 
-        let iconH = $icon.height(); 
-        let scale;
+$(function () {
+  $("iconify-icon").each(function () {
+    var point = window.innerWidth;
+    console.log(point);
+    let $icon = $(this).find('svg');
+    let iconW = $icon.width();
+    let iconH = $icon.height();
+    let scale;
 
-        if (point >= 1920) {
-            scale = 4;
-        } else if (point <= 1920 && point >= 1024) {
-            scale = 3;
-        } else if (point <= 1024 && point >= 480) {
-            scale = 2;
-        }
-        console.log(scale);
-        iconW *= scale;
-        iconH *= scale;
+    if (point >= 1920) {
+      scale = 4;
+    } else if (point <= 1920 && point >= 1024) {
+      scale = 3;
+    } else if (point <= 1024 && point >= 480) {
+      scale = 2;
+    }
+    console.log(scale);
+    iconW *= scale;
+    iconH *= scale;
 
-        // 將計算後的寬度和高度應用到 SVG 元素
-        $icon.width(iconW);
-        $icon.height(iconH);
-    });
+    // 將計算後的寬度和高度應用到 SVG 元素
+    $icon.width(iconW);
+    $icon.height(iconH);
+  });
 })
